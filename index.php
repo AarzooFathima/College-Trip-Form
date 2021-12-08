98ae55ef4fcfdba029083e913680f0d80a49d8d1
Aarzoo Fathima <64885566+AarzooFathima@users.noreply.github.com>
2021-12-08 11:44:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,40 @@
 <?php
-    $insert = false;
-
+$insert = false;
 if(isset($_POST['name'])){
+    // Set connection variables
     $server = "localhost";
     $username = "root";
     $password = "";
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
 
-    $con = mysqli_connect($server,$username,$password);
-    if(!$con)
-    {
-        die("Connection failed due to " . mysqli_connect_error());
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
     }
-
-    //echo "Succesful Connection";
+    // echo "Success connecting to the db";
+    // Collect post variables
     $name = $_POST['name'];
     $gender = $_POST['gender'];
     $age = $_POST['age'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $desc = $_POST['desc'];
-    $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+    $sql = "INSERT INTO `trip`.`trip`(`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+    // echo $sql;
 
+    // Execute the query
     if($con->query($sql) == true){
-        //echo "Successfully inserted";
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
         $insert = true;
     }
     else{
         echo "ERROR: $sql <br> $con->error";
     }
+
+    // Close the database connection
     $con->close();
 }
 ?>
@@ -36,32 +43,29 @@ if(isset($_POST['name'])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width", initial-scale="1.0">
     <title>Welcome to Travel Form</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <img class="bg" src="clg.jpg" alt="BIHE">
+    <img class="bg" src="clg.jpg" alt="College">
     <div class="container">
-        <h1>Welcome to BIHE DVG Chikmanglur Trip Form</h1>
-        <p>Enter your Details and Submit the form to confirm your participation in the trip</p>
+        <h1>Welcome to BIHE CHIKMANGLUR Trip form</h3>
+        <p>Enter your details and submit this form to confirm your participation in the trip </p>
         <?php
-            if($insert == true){
-                echo "<p class='submitMsg'>Thanks alot for Submitting...We are happy to see you joining us.</p>";
-            }
+        if($insert == true){
+        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the trip</p>";
+        }
         ?>
-        
         <form action="index.php" method="post">
             <input type="text" name="name" id="name" placeholder="Enter your name">
-            <input type="text" name="age" id="age" placeholder="Enter your age">
+            <input type="text" name="age" id="age" placeholder="Enter your Age">
             <input type="text" name="gender" id="gender" placeholder="Enter your gender">
             <input type="email" name="email" id="email" placeholder="Enter your email">
-            <input type="phone" name="phone" id="phone" placeholder="Enter your phone number">
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
             <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
-            <button class="btn">Submit</button>
-            <button class="btn">Reset</button>
+            <button class="btn">Submit</button> 
         </form>
-        
     </div>
     <script src="index.js"></script>
 </body>
